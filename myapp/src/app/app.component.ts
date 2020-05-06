0110107e0864a697521e743e2ebfcce4daa8c091
mpolague <mpolague@miners.utep.edu>
2020-05-05 22:01:06 -0600

    Installed and modified files to use the UI library PrimeNG

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';                  //api
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
